89ef32280d9174d848c9f60e7c9afd20a4beee77
LiChao <18810396626@163.com>
2023-11-20 21:38:36 +0800

    add write method

--- spring-iocdi-extends/src/com/j2ee/test/xml/Person.java
+++ spring-iocdi-extends/src/com/j2ee/test/xml/Person.java
@@ -1,9 +1,11 @@
 package com.j2ee.test.xml;
 
-public class Person extends Student{
+public class Person extends Student {
 
-	public void say() {
-		this.showName() ;
-	}
-	
+    public void say() {
+        this.showName();
+    }
+
+	public void write() {}
+
 }
